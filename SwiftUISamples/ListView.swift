b1e9080e6f915111288ebfad23e9ddbb88e77bf5
ayaka <arka.cigarette@gmail.com>
2024-07-10 23:47:35 +0900

    @Observable Model 추가

--- SwiftUISamples/ListView.swift
+++ SwiftUISamples/ListView.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
+struct Item: Hashable {
+    let id = UUID()
+    var name: String
+    var net: Float
+}
+
+@Observable class ItemModel: ObservableObject {
+    var items: [Item] = [
+            Item(name: "사과", net: 200.0),
+            Item(name: "우유", net: 1000.0),
+            Item(name: "계란", net: 60.0),
+            Item(name: "김치", net: 500.0)
+        ]
+}
+
 struct ListView: View {
     @Environment(\.dismiss) var dismiss
-    let items = ["사과", "우유", "계란", "김치"]
+    @StateObject private var model = ItemModel()
     
     var body: some View {
         NavigationStack {
-            List(items, id: \.self) { item in
-                NavigationLink(value: item) {
-                    Text(item)
+            List(model.items, id: \.self) { item in
+                NavigationLink(value: item.name) {
+                    Text(item.name)
+                    Text("\(item.net, specifier: "%.2f") g")
                 }
             }
             .navigationTitle(Text("ListView"))
